1ba3e8b56b3ad92308b69ed86057ba2a0d449849
ostgoth <andriybanit@gmail.com>
2016-10-13 02:11:35 +0000

    minor changes

--- function.py
+++ function.py
@@ -3,24 +3,23 @@ import json
 import datetime
 
 
-def summarise_forecast(name):
-    max_temp = -273
-    min_temp = 100500
+def summarise_forecast(city):
+    max = -273
+    min = 100500
     forecast = {}
-    r = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + name +
+    r = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city +
                      '&units=metric&cnt=14&appid=d5e66d1cb8eb964911baf723359c3f8d')
     for item in r.json()['list']:
-        if item['temp']['max'] > max_temp:
-            max_temp = item['temp']['max']
-        if item['temp']['min'] < min_temp:
-            min_temp = item['temp']['min']
+        if item['temp']['max'] > max:
+            max = item['temp']['max']
+        if item['temp']['min'] < min:
+            min = item['temp']['min']
         curr_date = datetime.datetime.fromtimestamp(item['dt']).strftime('%Y-%m-%d')
         if item['weather'][0]['main'] in forecast.keys():
             forecast[item['weather'][0]['main']].append(curr_date)
         else:
             forecast[item['weather'][0]['main']] = [curr_date]
-    return json.dumps({'city': name, 'max_temp': max_temp, 'min_temp': min_temp, 'forecasts': forecast},
-                      sort_keys=False, indent = 2, separators=(',', ':'))
-
+    return json.dumps({'city': city, 'max': max, 'min': min, 'forecasts': forecast},
+                      sort_keys=True, indent = 2, separators=(',', ':'))
 
 print(summarise_forecast('Barcelona'))
