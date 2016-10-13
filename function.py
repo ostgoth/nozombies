6c30bd5b8c2180b95d5e44f46aa8aa183ee48fcb
ostgoth <andriybanit@gmail.com>
2016-10-13 02:14:17 +0000

    minor changes

--- function.py
+++ function.py
@@ -6,7 +6,7 @@ import datetime
 def summarise_forecast(city):
     max = -273
     min = 100500
-    forecast = {}
+    forecasts = {}
     r = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city +
                      '&units=metric&cnt=14&appid=d5e66d1cb8eb964911baf723359c3f8d')
     for item in r.json()['list']:
@@ -16,10 +16,10 @@ def summarise_forecast(city):
             min = item['temp']['min']
         curr_date = datetime.datetime.fromtimestamp(item['dt']).strftime('%Y-%m-%d')
         if item['weather'][0]['main'] in forecast.keys():
-            forecast[item['weather'][0]['main']].append(curr_date)
+            forecasts[item['weather'][0]['main']].append(curr_date)
         else:
-            forecast[item['weather'][0]['main']] = [curr_date]
-    return json.dumps({'city': city, 'max': max, 'min': min, 'forecasts': forecast},
+            forecasts[item['weather'][0]['main']] = [curr_date]
+    return json.dumps({'city': city, 'max': max, 'min': min, 'forecasts': forecasts},
                       sort_keys=True, indent = 2, separators=(',', ':'))
 
 print(summarise_forecast('Barcelona'))
