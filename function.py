49fe3a32f2c0a0fc7b47c4b4afc5099ac09a71fb
ostgoth <andriybanit@gmail.com>
2016-10-13 01:20:55 +0000

    cleaning up

--- function.py
+++ function.py
@@ -1,42 +1,26 @@
 import requests
-#import json
-#from datetime import time
+import json
 import datetime
 
 
 def get_forecast(name):
     max_temp = -273
     min_temp = 100500
-    #clear_days = []
-    #rainy_days = []
     weather = {}
     r = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + name + '&units=metric&cnt=14&appid=d5e66d1cb8eb964911baf723359c3f8d')
     for item in r.json()['list']:
-        #print item['temp']['max']
         if item['temp']['max'] > max_temp:
             max_temp = item['temp']['max']
         if item['temp']['min'] < min_temp:
             min_temp = item['temp']['min']
-
-        #curr_date = time.strftime('%Y-%m-%d', time.localtime(item['dt']))
         curr_date = datetime.datetime.fromtimestamp(item['dt']).strftime('%Y-%m-%d')
         if item['weather'][0]['main'] in weather.keys():
             weather[item['weather'][0]['main']].append(curr_date)
         else:
             weather[item['weather'][0]['main']] = [curr_date]
-        """
-        if item['weather']['main'] == 'Clear':
-            clear_days.append(time.strftime('%Y-%m-%d', time.localtime(item['dt'])))
-        if item['weather']['main'] == 'Rain':
-            rainy_days.append(time.strftime('%Y-%m-%d', time.localtime(item['dt'])))
-        """
-    print 'max temp=', max_temp
-    print 'min temp=', min_temp
-    print 'weather=', weather
-    #print 'clear days are:', clear_days
-    #print 'clear days are:', rainy_days
-
-    #print (r.json())
-
+    print(name)
+    print('max temp =', max_temp)
+    print('min temp =', min_temp)
+    print('weather =', weather)
 
 get_forecast('Kyiv')
